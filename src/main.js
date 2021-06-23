8ee68ce04647f5c68bda010220b76cb96d19b7c6
RodgersX <brianmawira2@gmail.com>
2021-06-23 21:03:13 +0300

    FIX-001: date founded

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueMeta from 'vue-meta'
+
 import vuetify from './plugins/vuetify'
 import router from './router/index'
+
 
 // import common components
 import navBar from './components/navigation'
 import Footer from './components/footer'
+
+Vue.use(VueMeta, {
+  keyName: 'metaInfo',
+  refreshOnceOnNavigation: true
+})
 
 Vue.config.productionTip = false
 
